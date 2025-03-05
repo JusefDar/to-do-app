eff82056a6f13e97055371a0105593d29cdb295b
JusefDar <jusef.darwiche@tn.techstarter.de>
2025-03-05 12:54:51 +0100

    datenbank erstellt, post rewuest zum item hinzufügen und geschrieben

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const sqlite3 = require('sqlite3')
+const bodyParser = require('body-parser')
 
 const app = express();
 
+const db = new sqlite3.Database('./tasks.db')
+
+app.use(bodyParser.json())         // Middleware
+
+db.run('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)')
+
+db.run('INSERT INTO tasks (title) VALUES (?)', "Zähne putzen");
+
 app.get('/Jusef', (req, res)=> {
     res.send('vielen Dank!')
+})
+
+app.post('/add', () => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function (){
+        res.json({tag:"Mittwoch", bald_wirds: "Mittagspause"});
+    })
 })
 
 app.listen(3050, "localhost", () => {
