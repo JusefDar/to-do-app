77657edf744a8a72ddc01ffbea52c4ab527cde7e
JusefDar <jusef.darwiche@tn.techstarter.de>
2025-03-05 14:47:00 +0100

    datenbank erstellt, Beitragsanfrage zum Artikel hinzufügen geschrieben und getestet

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const sqlite3 = require('sqlite3')
-const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const db = new sqlite3.Database('./tasks.db')
+const db = new sqlite3.Database('./tasks.db');
 
-app.use(bodyParser.json())         // Middleware
+app.use(bodyParser.json());         // Middleware
 
-db.run('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)')
+db.run('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
 
 db.run('INSERT INTO tasks (title) VALUES (?)', "Zähne putzen");
 
-app.get('/Jusef', (req, res)=> {
+app.get('/', (req, res)=> {
     res.send('vielen Dank!')
-})
+});
+
+app.get('/Jusef', (req, res)=> {
+    res.send('Gibbbbbb Ihnnnn')
+});
 
 app.post('/add', () => {
     db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function (){
         res.json({tag:"Mittwoch", bald_wirds: "Mittagspause"});
     })
-})
+});
 
 app.listen(3050, "localhost", () => {
     console.log("bald wird es Mittagspause")
-})
+});
