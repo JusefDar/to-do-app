ae0a0d1fd4b7ba8e5f458e3c5e5cc56329136c54
JusefDar <jusef.darwiche@tn.techstarter.de>
2025-03-06 10:16:37 +0100

    liste_abrufen GET Request hinzugefügt

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,44 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//TODO: Verbinde eine Datenbank dazu
+
 const db = new sqlite3.Database('./tasks.db');
 
-app.use(bodyParser.json());         // Middleware
+app.use(bodyParser.json());     // Middleware (wie ein Übersetzer)
 
-db.run('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
 
-db.run('INSERT INTO tasks (title) VALUES (?)', "Zähne putzen");
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
 
-app.get('/', (req, res)=> {
-    res.send('vielen Dank!')
+
+//TODO: Schreibe requests/responses
+
+
+app.get('/', (req, res) => {
+    res.send('request received');
 });
 
-app.get('/Jusef', (req, res)=> {
-    res.send('Gibbbbbb Ihnnnn')
+app.get('/ralf', (req, res) => {
+    res.send('vielen Dank Ralf');
 });
 
-app.post('/add', () => {
-    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function (){
-        res.json({tag:"Mittwoch", bald_wirds: "Mittagspause"});
+// Wenn ein neues Item hinzugefügt werden soll, soll NodeJS Server diesen Request so behandeln:
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
+});
+
+
+// Liste mir alle existierende Items
+// hier sollte nur alle Items als JSON im Response geschrieben werden
+app.get('/liste_abrufen', (req, res) => {
+    db.all('SELECT * FROM tasks', function (err, rows){
+        res.json(rows);
     })
 });
+
+
 
 app.listen(3050, "localhost", () => {
     console.log("bald wird es Mittagspause")
